a9bcc1f87260c188e29824664fbdf06a37a29344
Thirumalesh Chinna <thirumalesh.chinna@talentica.com>
2017-05-20 23:21:27 +0530

    added on click functionality

--- musicBox/music.js
+++ musicBox/music.js
@@ -1,23 +1,22 @@
   function removeTransition(e) {
-    if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing');
+      if (e.propertyName !== 'transform') return;
+      e.target.classList.remove('playing');
   }
 
   function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    if (!audio) return;
-    key.addEventListener('transitionend', removeTransition);
-
-    key.classList.add('playing');
-    audio.currentTime = 0;
-    audio.play();
+      var code = e.keyCode && e.keyCode || e;
+      const audio = document.querySelector(`audio[data-key="${code}"]`);
+      const key = document.querySelector(`div[data-key="${code}"]`);
+      if (!audio) return;
+      key.classList.add('playing');
+      audio.currentTime = 0;
+      audio.play();
   }
-
+  var x = document.getElementsByClassName("key");
+  x.onclick = playSound;
   const keys = Array.from(document.querySelectorAll('.key'));
   keys.forEach(key => {
-      console.log("hey");
-      return key.addEventListener('transitionend', removeTransition);
-      
+      key.addEventListener('transitionend', removeTransition);
+      key.onclick = playSound.bind(null, key.getAttribute("data-key"));
   });
   window.addEventListener('keydown', playSound);
